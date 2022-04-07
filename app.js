f994a762a627321e0a6194adace8dbff1fcc71fa
ddnold <s532534@nwmissouri.edu>
2022-04-06 20:19:25 -0500

    Part 8 endpoint.

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var FarmhandsRouter = require('./routes/Farmhands');
-var addmodsRouter = require('./routes/addmods')
+var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -24,7 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Farmhands', FarmhandsRouter);
-app.use('/addmods', addmodsRouter)
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
